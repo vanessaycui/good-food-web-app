6eb7902c25b3b0e45273e6432608bd0fbffe0917
Vanessa <vanessa.y.cui@gmail.com>
2022-08-14 01:28:56 -0400

    update

--- foodCollection.py
+++ foodCollection.py
@@ -29,7 +29,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-del os.environ['DATABASE_URL']
+
 # *** DATABASE TABLES ***
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
