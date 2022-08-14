bc407625257e3bb873d0f37b8d44997ae21cf682
Vanessa <vanessa.y.cui@gmail.com>
2022-08-14 02:18:41 -0400

    update

--- foodCollection.py
+++ foodCollection.py
@@ -25,7 +25,7 @@ import os
 # *** CONFIGURE FLASK APPLICATION ***
 app = Flask(__name__)
 #Optional: But it will silence the deprecation warning in the console.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL2", "sqlite:///food-collection.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("postgresql://bmuglyuqbysfju:23582e71cc7a70ecb560a544afb273c13ae944eebf4f567ddaa76d21bef032d3@ec2-44-195-100-240.compute-1.amazonaws.com:5432/df6ov0moi33t60", "sqlite:///food-collection.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
