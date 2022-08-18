dbed57c820f22d2fb15ac4bfbab10b054f9d0f6e
Vanessa <vanessa.y.cui@gmail.com>
2022-08-17 20:58:10 -0400

    updated tables to blob for images

--- foodCollection.py
+++ foodCollection.py
@@ -62,7 +62,7 @@ class Recipe(db.Model):
     ingredients = db.relationship("Ingredient", backref="recipe")
     # setting these as nullable=true for now, because there exists recipes w/o type and image rn. may modify later.
     foodType = db.Column(db.String(80), nullable=True)
-    image = db.Column(db.VARCHAR, nullable=True)
+    image = db.Column(db.LargeBinary, nullable=True)
     # return a dictionary as a method in class Recipe
     def to_dict(self):
         # for each column in table, set key as name of column and value is value of column. 
@@ -164,7 +164,7 @@ def webAddRecipe():
             with BytesIO() as buf:
                 img.save(buf, 'png')
                 image_bytes = buf.getvalue()
-            encoded_string = base64.b64encode(image_bytes).decode("ASCII", "ignore")
+            encoded_string = base64.b64encode(image_bytes)
             
         newRecipe = Recipe(
             title = request.form.get("recipeTitle"),
